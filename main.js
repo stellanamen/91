4ec1182918765cbe9b5b1291bd662a00b21dd40a
stellanamen <121315996+stellanamen@users.noreply.github.com>
2023-08-23 16:50:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function send(){
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+
+    question_number = "<h4>" +number +"X" +number2  +"</h4>";
+    input_box = "<br>Resposta : <input type= 'text' id='_check_box>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'> Checar</button>"; 
+    row = question_number+input_box+check_button;
+    
+    document.getElementById("ontput").innerHTML = row;
+document.getElementById("number1").value ="";
+document.getElementById("number2").value ="";
+}
